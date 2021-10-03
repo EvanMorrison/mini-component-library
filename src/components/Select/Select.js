35f88c71204c41b30e0b0b2a58e4707f50bdc12f
Evan Morrison <evanmorrison@filevine.com>
2021-10-02 18:04:35 -0600

    select

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -9,25 +9,61 @@ const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <StyledSelect value={value} onChange={onChange}>
-      {children}
-    </StyledSelect>
+    <Wrapper>
+      <NativeSelect value={value} onChange={onChange}>
+        {children}
+      </NativeSelect>
+      <PresentationalSelect displayedValue={displayedValue}>
+        <span>{displayedValue}</span><SelectIcon id='chevron-down' size={24} />
+      </PresentationalSelect>
+    </Wrapper>
   );
 };
 
-const StyledSelect = styled.select`
+const Wrapper = styled.div`
+  position: relative;
+  max-width: max-content;
   color: ${COLORS.gray700};
   font-size: 1rem;
-  background-color: ${COLORS.transparentGray15};
-  width: min-content;
-  min-width: 0;
-  border-radius: 8px;
-  padding: 10px 16px 12px;
-  appearance: none;
+`;
 
-  &:hover {
+const NativeSelect = styled.select`
+  appearance: none;
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  opacity: 0;
+`;
+
+const PresentationalSelect = styled.div`
+  color: inherit;
+  display: block;
+  background-color: ${COLORS.transparentGray15};
+  border-radius: 8px;
+  padding: 12px 16px;
+  padding-right: 48px;
+
+  ${NativeSelect}:hover + & {
     color: ${COLORS.black};
   }
+
+  ${NativeSelect}:focus + & {
+    outline: solid ${COLORS.black};
+    outline: solid -webkit-focus-ring-color;
+  }
+`;
+
+const SelectIcon = styled(Icon)`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  margin: auto;
+  height: ${p => p.size}px;
+  width: ${p => p.size}px;
+  right: 10px;
+  pointer-events: none;
 `;
 
 export default Select;
